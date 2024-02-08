84eec93adca2476c612a63a7661d043d284a9fac
alijr2018 <alijr_2018@proton.me>
2024-02-08 19:30:51 +0100

    fix

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -12,3 +12,7 @@ def hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
