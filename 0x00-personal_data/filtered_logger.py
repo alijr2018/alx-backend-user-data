2c9bd83c171ea3f9392d4f888728e57862ed866a
alijr2018 <alijr_2018@proton.me>
2024-02-08 19:21:41 +0100

    check

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -33,26 +33,29 @@ class RedactingFormatter(logging.Formatter):
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
     """The function use a regex to replace occurrences field values."""
-    return re.sub(fr'(\b(?:{"|".join(fields)})=)[^{separator}]+',
+    for field in fields:
+        msg = re.sub(fr'(\b(?:{"|".join(fields)})=)[^{separator}]+',
                   fr'\1{redaction}', message)
+        return msg
 
 
 PII_FIELDS = ("name", "email", "phone", "ssn", "credit_card")
 
 
 def get_logger() -> logging.Logger:
-    """ Returns a logging.Logger object """
+    """
+    function that takes no arguments and returns a logging.Logger object.
+    """
     logger = logging.getLogger("user_data")
     logger.setLevel(logging.INFO)
     logger.propagate = False
 
-    target_handler = logging.StreamHandler()
-    target_handler.setLevel(logging.INFO)
-
+    stream_handler = logging.StreamHandler()
     formatter = RedactingFormatter(list(PII_FIELDS))
-    target_handle.setFormatter(formatter)
+    stream_handler.setFormatter(formatter)
 
-    logger.addHandler(target_handler)
+    logger.addHandler(stream_handler)
+
     return logger
 
 
@@ -73,3 +76,27 @@ def get_db() -> MySQLConnection:
     )
 
     return db
+
+def main():
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    stream_handler = logging.StreamHandler()
+    formatter = RedactingFormatter(fields=PII_FIELDS)
+    stream_handler.setFormatter(formatter)
+
+    logger.addHandler(stream_handler)
+
+    db = get_db()
+    cursor = db.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM users;")
+    for row in cursor:
+        log_message = "; ".join(f"{key}={row[key]}" for key in row)
+        logger.info(log_message)
+
+    cursor.close()
+    db.close()
+
+if __name__ == "__main__":
+    main()
