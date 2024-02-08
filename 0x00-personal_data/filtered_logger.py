5f87037ef7307d013a83e822555e1ca7446fce1b
alijr2018 <alijr_2018@proton.me>
2024-02-08 10:37:30 +0100

    check

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,5 +4,6 @@ import re
 
 
 def filter_datum(fields, redaction, message, separator):
+    """The function use a regex to replace occurrences field values."""
     return re.sub(fr'(\b(?:{"|".join(fields)})=)[^{separator}]+',
                   fr'\1{redaction}', message)
