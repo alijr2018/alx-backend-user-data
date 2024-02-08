f8bd4e1735d4eefd90eaeae25f3672e21b8192e8
alijr2018 <alijr_2018@proton.me>
2024-02-08 10:54:55 +0100

    check[5~

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,6 +1,26 @@
 #!/usr/bin/env python3
 """filtered_logger.py"""
 import re
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        NotImplementedError
+        log_message = super().format(record)
+        return filter_datum(self.fields,
+                            self.REDACTION, log_message, self.SEPARATOR)
 
 
 def filter_datum(fields, redaction, message, separator):
