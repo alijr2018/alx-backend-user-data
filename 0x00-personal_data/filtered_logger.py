fab06f3711fe08d779baae19c362163f46fcd673
alijr2018 <alijr_2018@proton.me>
2024-02-08 12:36:47 +0100

    2. Create logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -31,3 +31,23 @@ def filter_datum(fields: List[str], redaction: str, message: str,
     """The function use a regex to replace occurrences field values."""
     return re.sub(fr'(\b(?:{"|".join(fields)})=)[^{separator}]+',
                   fr'\1{redaction}', message)
+
+
+PII_FIELDS = ("name", "email", "phone", "ssn", "credit_card")
+
+
+def get_logger():
+    """
+    function that takes no arguments and returns a logging.Logger object.
+    """
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    stream_handler = logging.StreamHandler()
+    formatter = RedactingFormatter(fields=PII_FIELDS)
+    stream_handler.setFormatter(formatter)
+
+    logger.addHandler(stream_handler)
+
+    return logger
