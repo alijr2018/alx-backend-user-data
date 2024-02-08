e2971bb589216e06f308757df4d9dd4117e84b7d
alijr2018 <alijr_2018@proton.me>
2024-02-08 10:35:11 +0100

    check

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,5 +6,5 @@ import re
 
 
 def filter_datum(fields, redaction, message, separator):
-    return re.sub(
-        fr'(?<=\b({"|".join(fields)})=).*?(?={separator})', redaction, message)
+    return re.sub(fr'(\b(?:{"|".join(fields)})=)[^{separator}]+',
+                  fr'\1{redaction}', message)
