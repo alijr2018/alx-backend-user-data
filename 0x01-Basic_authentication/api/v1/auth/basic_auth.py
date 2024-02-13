f34d1327c9bb2d21ad558136e0b58d6f0e456083
alijr2018 <alijr_2018@proton.me>
2024-02-13 21:19:57 +0100

    check

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -36,10 +36,24 @@ class BasicAuth(Auth):
             return None
 
         try:
-            # Decode Base64 and return as UTF-8 string
             decoded_credentials = base64.b64decode(base64_authorization_header)
             decoded_credentials_utf8 = decoded_credentials.decode('utf-8')
             return decoded_credentials_utf8
         except Exception as e:
-            # Handle decoding errors
             return None
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
+        """
+        returns the user email and password from the Base64 decoded value.
+        """
+        if (
+            decoded_base64_authorization_header is None or
+            not isinstance(decoded_base64_authorization_header, str) or
+            ':' not in decoded_base64_authorization_header
+        ):
+            return None, None
+
+        user_email, user_password = decoded_base64_authorization_header.split(
+            ':', 1)
+        return user_email, user_password
