813bcb539301ba3d7e5d2cec4cfeb7a4e4eb7fb9
alijr2018 <alijr_2018@proton.me>
2024-02-13 11:16:55 +0100

    check

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,10 +2,28 @@
 """ Module of basic_auth
 """
 from .auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
     """
     class BasicAuth
     """
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """
+        returns the Base64 part of,
+        the Authorization header for a Basic Authentication
+        """
+        if authorization_header is None or not isinstance(authorization_header, str):
+            return None
+
+        if not authorization_header.startswith("Basic "):
+            return None
+
+        base64_credentials = authorization_header.split(" ", 1)[1]
+        try:
+            decoded_credentials = base64.b64decode(base64_credentials).decode('utf-8')
+            return decoded_credentials
+        except Exception as e:
+            return None
