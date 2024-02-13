d60dbe19fd5b435cd4b9006046e0acfb3a2970fc
alijr2018 <alijr_2018@proton.me>
2024-02-13 12:16:19 +0100

    check

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -25,3 +25,21 @@ class BasicAuth(Auth):
         base64_credentials = authorization_header.split(" ", 1)[1]
 
         return base64_credentials
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """Decode the Base64 authorization header to UTF-8."""
+        if (
+            base64_authorization_header is None or
+            not isinstance(base64_authorization_header, str)
+        ):
+            return None
+
+        try:
+            # Decode Base64 and return as UTF-8 string
+            decoded_credentials = base64.b64decode(base64_authorization_header)
+            decoded_credentials_utf8 = decoded_credentials.decode('utf-8')
+            return decoded_credentials_utf8
+        except Exception as e:
+            # Handle decoding errors
+            return None
