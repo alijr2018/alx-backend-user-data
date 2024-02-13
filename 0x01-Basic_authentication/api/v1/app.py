5b6bc9e572df8e7d2abb365c7d7be258c1e89dc4
alijr2018 <alijr_2018@proton.me>
2024-02-13 10:59:47 +0100

    fix

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -15,6 +15,7 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
+
 if "AUTH_TYPE" in os.environ:
     auth_type = os.environ["AUTH_TYPE"]
     if auth_type == "basic_auth":
