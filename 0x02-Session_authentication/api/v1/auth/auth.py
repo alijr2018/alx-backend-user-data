1341d22e9af4168d5e2487ff1f9444b9e886faf9
alijr2018 <alijr_2018@proton.me>
2024-02-14 10:56:47 +0100

    set-up

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+""" Module of auth
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """
+    Auth class.
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        returns False - path
+        """
+        if path is None or not excluded_paths:
+            return True
+
+        path_with_slash = path if path.endswith("/") else path + "/"
+
+        for excluded_path in excluded_paths:
+            if "*" in excluded_path:
+                prefix = excluded_path.rstrip("*")
+                if path.startswith(prefix):
+                    return False
+            elif path_with_slash == excluded_path or path == excluded_path:
+                return False
+
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """
+         returns None - request
+        """
+        if request is not None:
+            return request.headers.get('Authorization', None)
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        returns None - request
+        """
+        return None
