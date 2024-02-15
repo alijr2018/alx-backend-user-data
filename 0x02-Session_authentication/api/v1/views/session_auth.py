64c24c51e188e1c83754b7485866ba1a47136f99
alijr2018 <alijr_2018@proton.me>
2024-02-15 10:43:47 +0100

    cjecl

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -7,29 +7,32 @@ from typing import Tuple
 from flask import jsonify, request, abort
 from api.v1.views import app_views
 from models.user import User
-from api.v1.app import auth
 
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
-def session_login() -> Tuple[str, int]:
+def session_login():
     """
     Session login route
     """
-    email = request.form.get('email')
-    password = request.form.get('password')
-    if email is None or email == '':
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    if not email:
         return jsonify({"error": "email missing"}), 400
-    if password is None or password == '':
+
+    if not password:
         return jsonify({"error": "password missing"}), 400
-    users = User.search({"email": email})
-    if not users or users == []:
+
+    user = User.search({"email": email})
+    if not user:
         return jsonify({"error": "no user found for this email"}), 404
-    for user in users:
-        if user.is_valid_password(password):
-            from api.v1.app import auth
-            session_id = auth.create_session(user.id)
-            resp = jsonify(user.to_json())
-            session_name = os.getenv('SESSION_NAME')
-            resp.set_cookie(session_name, session_id)
-            return resp
-    return jsonify({"error": "wrong password"}), 401
+
+    if not user[0].is_valid_password(password):
+        return jsonify({"error": "wrong password"}), 401
+
+    session_id = auth.create_session(user[0].id)
+    user_dict = user[0].to_json()
+    response = jsonify(user_dict)
+    response.set_cookie(auth.session_cookie_name, session_id)
+
+    return response
