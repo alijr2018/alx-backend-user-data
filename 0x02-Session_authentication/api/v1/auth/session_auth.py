aa1daa03d05ce81933f014a04df47c36387ed15e
alijr2018 <alijr_2018@proton.me>
2024-02-17 11:06:23 +0100

    fix

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -14,6 +14,9 @@ class SessionAuth(Auth):
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
+        """
+        create a session
+        """
         if user_id is None or not isinstance(user_id, str):
             return None
 
