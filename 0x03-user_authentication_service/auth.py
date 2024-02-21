0c1ec4e58fcb4f34c3579368b9c5277eb473f4ff
alijr2018 <alijr_2018@proton.me>
2024-02-21 20:31:01 +0100

    4

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,36 +1,10 @@
-#!/usr/bin/env python3
-"""
-auth.py
-"""
+# auth module
 import bcrypt
-from db import DB
-from user import User
-from _hash_password import _hash_password
 
 
 def _hash_password(password: str) -> bytes:
-    """
-    Hash a password with salt using bcrypt
+    """Hash a password with salt using bcrypt
     """
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password
-
-
-class Auth:
-    """Auth class to interact with the authentication database.
-    """
-
-    def __init__(self):
-        self._db = DB()
-
-    def register_user(self, email: str, password: str) -> User:
-        """Register a new user and return the User object.
-        """
-        try:
-            self._db.find_user_by(email=email)
-            raise ValueError("User {} already exists".format(email))
-        except ValueError:
-            hashed_password = _hash_password(password)
-            user = self._db.add_user(email, hashed_password)
-            return user
