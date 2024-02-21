864f086b1fee181d28572f21a8bd60a3e75cdf44
alijr2018 <alijr_2018@proton.me>
2024-02-21 20:40:23 +0100

    checking

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -44,11 +44,14 @@ class DB:
         """
         Find a user
         """
-        user = self._session.query(User).filter_by(**kwargs).first()
-        if user is None:
-            raise NoResultFound("No user found")
-        else:
-            return user
+        session = self._session
+        try:
+            user = session.query(User).filter_by(**kwargs).one()
+        except NoResultFound:
+            raise NoResultFound()
+        except InvalidRequestError:
+            raise InvalidRequestError()
+        return user
 
     def update_user(self, user_id: int, **kwargs):
         """
