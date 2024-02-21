366299941a69e31d799c39723389c42105e2912b
alijr2018 <alijr_2018@proton.me>
2024-02-21 20:29:40 +0100

    check

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,6 +3,9 @@
 auth.py
 """
 import bcrypt
+from db import DB
+from user import User
+from _hash_password import _hash_password
 
 
 def _hash_password(password: str) -> bytes:
@@ -12,3 +15,22 @@ def _hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Register a new user and return the User object.
+        """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError("User {} already exists".format(email))
+        except ValueError:
+            hashed_password = _hash_password(password)
+            user = self._db.add_user(email, hashed_password)
+            return user
