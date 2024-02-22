46008d0d4440806058da3776ab660d59227d3de7
alijr2018 <alijr_2018@proton.me>
2024-02-22 16:55:19 +0100

    fixing

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -44,20 +44,13 @@ class DB:
         """
         Find a user
         """
-        if not kwargs:
-            raise InvalidRequestError
-
-        column_names = User.__table__.columns.keys()
-        for key in kwargs.keys():
-            if key not in column_names:
-                raise InvalidRequestError
-
-        user = self._session.query(User).filter_by(**kwargs).first()
-
-        if user is None:
+        try:
+            user = self._session.query(User).filter_by(**kwargs).one()
+            return user
+        except NoResultFound:
             raise NoResultFound
-
-        return user
+        except InvalidRequestError:
+            raise InvalidRequestError("Invalid request")
 
     def update_user(self, user_id: int, **kwargs) -> None:
         """
@@ -65,18 +58,11 @@ class DB:
         """
         try:
             user = self.find_user_by(id=user_id)
+            for key, value in kwargs.items():
+                if hasattr(User, key):
+                    setattr(user, key, value)
+                else:
+                    raise ValueError("Invalid attribute: {}".format(key))
+            self._session.commit()
         except NoResultFound:
             raise ValueError("No user found with id: {}".format(user_id))
-
-        column_names = User.__table__.columns.keys()
-        for key in kwargs.keys():
-            if key not in column_names:
-                raise ValueError("Invalid attribute: {}".format(key))
-
-        for key, value in kwargs.items():
-            setattr(user, key, value)
-
-        try:
-            self._session.commit()
-        except InvalidRequestError:
-            raise ValueError("Invalid request")
