98b2463a6d9be1ee197576fbeba9ef080fa1fb76
alijr2018 <alijr_2018@proton.me>
2024-02-22 17:28:14 +0100

    fixing

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -55,10 +55,16 @@ class Auth:
         """
         try:
             user = self._db.find_user_by(email=email)
-            return bcrypt.checkpw(password.encode('utf-8'),
-                                  user.hashed_password)
         except NoResultFound:
             return False
+
+        user_password = user.hashed_password
+        encoded_password = password.encode()
+
+        if bcrypt.checkpw(encoded_password, user_password):
+            return True
+
+        return False
 
     def create_session(self, email: str) -> str:
         """
